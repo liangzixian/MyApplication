560c4ca60f48ca7d7ac7503e08b726c433e55fb7
liangzixian <867488256@qq.com>
2016-06-23 11:18:38 +0800

    Initial commit
    (cherry picked from commit 5e8d74bc1a12c6720058f62bb77cbe1c41f02fc9)

---
+++ app/src/main/java/com/mrliang/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mrliang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
